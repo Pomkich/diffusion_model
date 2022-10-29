fb2a270609556045b59e7793603e4ced5aac2f01
Роман Смирнов <pombl4.01@mail.ru>
2022-10-29 17:40:32 +0300

    added thermal calculating

--- diffusion_model/main.cpp
+++ diffusion_model/main.cpp
@@ -3,9 +3,14 @@
 #include <iostream>
 #include <sstream>
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 
 typedef std::vector<std::vector<int>> array;
 typedef std::shared_ptr<std::vector<std::vector<int>>> array_pt;
+
+int clamp(int n, int lower, int upper) {
+    return std::max(lower, std::min(n, upper));
+}
 
 void read_file(std::string file_name, std::vector<std::vector<int>>& arr) {
     std::ifstream file(file_name);
@@ -21,56 +26,70 @@ void read_file(std::string file_name, std::vector<std::vector<int>>& arr) {
         arr.push_back(std::vector<int>());
         i++;
     }
+    arr.pop_back();
 }
 
 int main()
 {
+    // buffers contain start temperature
     array_pt first_buffer = std::make_shared<array>();
     array_pt second_buffer = std::make_shared<array>();
+    array conductivity_coef;
+    array thermal_points;
+
     read_file("massiv.txt", *first_buffer);
     read_file("massiv.txt", *second_buffer);
 
-    (*first_buffer)[0][0] = 12;
-    (*second_buffer)[0][0] = 3;
-
+    // init conductivity and thermal points
     for (int i = 0; i < first_buffer->size(); i++) {
+        conductivity_coef.push_back(std::vector<int>());
+        thermal_points.push_back(std::vector<int>());
         for (int j = 0; j < (*first_buffer)[i].size(); j++) {
-            std::cout << (*first_buffer)[i][j] << " ";
+            conductivity_coef[i].push_back(1);
+            thermal_points[i].push_back(0);
         }
-        std::cout << std::endl;
     }
 
-    std::cout << "~~~~~~~~~~~~~~~~~" << std::endl;
+    thermal_points[0][3] = 1;
 
-    for (int i = 0; i < second_buffer->size(); i++) {
-        for (int j = 0; j < (*second_buffer)[i].size(); j++) {
-            std::cout << (*second_buffer)[i][j] << " ";
+    // init constants
+    const int h = 1;
+    const int dt = 1;
+
+    int pause = 0;
+    for (int x = 0; x < 10; x++) {
+        for (int i = 0; i < first_buffer->size(); i++) {
+            for (int j = 0; j < (*first_buffer)[i].size(); j++) {
+                // avoiding going outside the array
+                int x_prev, y_prev;
+                int x_next, y_next;
+                x_prev = i - 1; y_prev = j - 1;
+                x_next = i + 1; y_next = j + 1;
+                x_prev = clamp(x_prev, 0, first_buffer->size() - 1);
+                y_prev = clamp(y_prev, 0, (*first_buffer)[i].size() - 1);
+                x_next = clamp(x_next, 0, first_buffer->size() - 1);
+                y_next = clamp(y_next, 0, (*first_buffer)[i].size() - 1);
+
+                // calculation of the next iteration
+                (*second_buffer)[i][j] = (*first_buffer)[i][j] + conductivity_coef[i][j] * (
+                    (*first_buffer)[i][y_prev] + (*first_buffer)[i][y_next] +
+                    (*first_buffer)[x_prev][j] + (*first_buffer)[x_next][j]) / 
+                    (h * h) * dt + thermal_points[i][j] * dt;
+            }
         }
-        std::cout << std::endl;
-    }
 
-    //switch_buffers(first_buffer, second_buffer);
-    std::swap(first_buffer, second_buffer);
-    std::cout << "%%%%%%%%%%%%%%%%%%%%%" << std::endl;
-
-    for (int i = 0; i < first_buffer->size(); i++) {
-        for (int j = 0; j < (*first_buffer)[i].size(); j++) {
-            std::cout << (*first_buffer)[i][j] << " ";
+        for (int i = 0; i < second_buffer->size(); i++) {
+            for (int j = 0; j < (*second_buffer)[i].size(); j++) {
+                std::cout << (*second_buffer)[i][j] << " ";
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
+
+        // swtich buffers
+        std::swap(first_buffer, second_buffer);
+
+        std::cin >> pause;
     }
-
-    std::cout << "~~~~~~~~~~~~~~~~~" << std::endl;
-
-    for (int i = 0; i < second_buffer->size(); i++) {
-        for (int j = 0; j < (*second_buffer)[i].size(); j++) {
-            std::cout << (*second_buffer)[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    //const delta_time = 1;
-
 
     return 0;
 }
