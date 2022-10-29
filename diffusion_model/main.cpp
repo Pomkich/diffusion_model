d325c06d4dd81c1f487d94d92bb755747a815d9c
Роман Смирнов <pombl4.01@mail.ru>
2022-10-29 18:52:22 +0300

    added visualization

--- diffusion_model/main.cpp
+++ diffusion_model/main.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <sstream>
 #include <SFML/Graphics.hpp>
-#include <algorithm>
+#include <thread>
 
+typedef std::vector<std::vector<sf::RectangleShape>> array_rect;
 typedef std::vector<std::vector<int>> array;
 typedef std::shared_ptr<std::vector<std::vector<int>>> array_pt;
 
@@ -29,6 +30,38 @@ void read_file(std::string file_name, std::vector<std::vector<int>>& arr) {
     arr.pop_back();
 }
 
+void calculate_therm(array_pt& prev_iter, array_pt& next_iter, array& con_coef, array& therm_pts) {
+    // init constants
+    const int h = 1;
+    const int dt = 1;
+
+    int x_prev, y_prev;
+    int x_next, y_next;
+
+    for (int i = 0; i < prev_iter->size(); i++) {
+        for (int j = 0; j < (*prev_iter)[i].size(); j++) {
+            // avoiding going outside the array
+            x_prev = i - 1; y_prev = j - 1;
+            x_next = i + 1; y_next = j + 1;
+            x_prev = clamp(x_prev, 0, prev_iter->size() - 1);
+            y_prev = clamp(y_prev, 0, (*prev_iter)[i].size() - 1);
+            x_next = clamp(x_next, 0, prev_iter->size() - 1);
+            y_next = clamp(y_next, 0, (*prev_iter)[i].size() - 1);
+
+            // calculation of the next iteration
+            (*next_iter)[i][j] = (*prev_iter)[i][j] + con_coef[i][j] * (
+                (*prev_iter)[i][y_prev] + (*prev_iter)[i][y_next] +
+                (*prev_iter)[x_prev][j] + (*prev_iter)[x_next][j] + 
+                (*prev_iter)[i][j]) / 5 / 
+                (h * h) * dt + therm_pts[i][j] * dt;
+
+            if ((*next_iter)[i][j] > 255) {
+                (*next_iter)[i][j] = 255;
+            }
+        }
+    }
+}
+
 int main()
 {
     // buffers contain start temperature
@@ -50,45 +83,64 @@ int main()
         }
     }
 
-    thermal_points[0][3] = 1;
+    // first 20 cells have conductivity = 2
+    for (int i = 0; i < 20; i++) {
+        conductivity_coef.push_back(std::vector<int>());
+        thermal_points.push_back(std::vector<int>());
+        for (int j = 0; j < conductivity_coef[i].size(); j++) {
+            conductivity_coef[i][j] = 2;
+        }
+    }
+    
+    // setup thermal points
+    //thermal_points[10][10] = 1;
+    thermal_points[50][30] = 1;
 
-    // init constants
-    const int h = 1;
-    const int dt = 1;
+    const int screen_width = 800;
+    const int screen_height = 600;
+
+    sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "thermal simulation");
+    // init rectagnles
+    array_rect rectangles;
+    for (int i = 0; i < first_buffer->size(); i++) {
+        rectangles.push_back(std::vector<sf::RectangleShape>());
+        for (int j = 0; j < (*first_buffer)[i].size(); j++) {
+            sf::RectangleShape rect;
+            rect.setSize(sf::Vector2f(10, 10));
+            rect.setFillColor(sf::Color::Black);
+            rect.setPosition(j * 10, i * 10);
+            rectangles[i].push_back(rect);
+        }
+    }
 
     int pause = 0;
-    for (int x = 0; x < 10; x++) {
-        for (int i = 0; i < first_buffer->size(); i++) {
-            for (int j = 0; j < (*first_buffer)[i].size(); j++) {
-                // avoiding going outside the array
-                int x_prev, y_prev;
-                int x_next, y_next;
-                x_prev = i - 1; y_prev = j - 1;
-                x_next = i + 1; y_next = j + 1;
-                x_prev = clamp(x_prev, 0, first_buffer->size() - 1);
-                y_prev = clamp(y_prev, 0, (*first_buffer)[i].size() - 1);
-                x_next = clamp(x_next, 0, first_buffer->size() - 1);
-                y_next = clamp(y_next, 0, (*first_buffer)[i].size() - 1);
-
-                // calculation of the next iteration
-                (*second_buffer)[i][j] = (*first_buffer)[i][j] + conductivity_coef[i][j] * (
-                    (*first_buffer)[i][y_prev] + (*first_buffer)[i][y_next] +
-                    (*first_buffer)[x_prev][j] + (*first_buffer)[x_next][j]) / 
-                    (h * h) * dt + thermal_points[i][j] * dt;
-            }
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
 
-        for (int i = 0; i < second_buffer->size(); i++) {
-            for (int j = 0; j < (*second_buffer)[i].size(); j++) {
-                std::cout << (*second_buffer)[i][j] << " ";
+        window.clear();
+        for (int i = 0; i < rectangles.size(); i++) {
+            for (int j = 0; j < rectangles[i].size(); j++) {
+                window.draw(rectangles[i][j]);
             }
-            std::cout << std::endl;
         }
-
-        // swtich buffers
-        std::swap(first_buffer, second_buffer);
-
-        std::cin >> pause;
+        window.display();
+        // recalculate temperature
+        calculate_therm(first_buffer, second_buffer, conductivity_coef, thermal_points);
+        for (int i = 0; i < rectangles.size(); i++) {
+            for (int j = 0; j < rectangles[i].size(); j++) {
+                sf::Color color;    // setup new color
+                color.r = (*second_buffer)[i][j];
+                rectangles[i][j].setFillColor(color);
+            }
+        }
+        swap(first_buffer, second_buffer);
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     return 0;
